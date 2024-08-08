fbf2637d92ba184a992676595cd8036f8be8c471
kim <scilo200@gmail.com>
2024-08-09 03:34:24 +0900

    swagger 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,5 +10,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation(project(":domain"))
 }
