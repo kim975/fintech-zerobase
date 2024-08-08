549c83510b7eae469659394845886958115998ab
kim <scilo200@gmail.com>
2024-08-08 19:04:05 +0900

    domain 추가

---
+++ domain/src/main/kotlin/com/zerobase/domain/domain/LoanReview.kt
@@ -0,0 +1,23 @@
+package com.zerobase.domain.domain
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "LOAN_REVIEW")
+class LoanReview(
+    @Column(name = "usr_key")
+    val userKey: String,
+
+    @Column(name = "loan_lmt_amt")
+    val loanLimitedAmount: Long,
+
+    @Column(name = "loan_intrt")
+    val loanInterest: Double
+
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+}
