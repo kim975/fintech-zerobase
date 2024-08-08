5c848fa40af9861c5c5ca07075474a5a3c1c7dca
kim <scilo200@gmail.com>
2024-08-09 03:34:07 +0900

    loan review 개발

--- domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
+++ domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
@@ -2,5 +2,8 @@ package com.zerobase.domain.repository
 
 import com.zerobase.domain.domain.LoanReview
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface LoanReviewRepository : JpaRepository<LoanReview, Long>
+interface LoanReviewRepository : JpaRepository<LoanReview, Long> {
+    fun findByUserKey(userKey: String): LoanReview
+}
